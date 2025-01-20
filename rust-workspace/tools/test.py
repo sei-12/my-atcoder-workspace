cf80ef16c98229479dd387c7bbe8fb0fcf12b891
sei-12 <sei.t11@icloud.com>
2025-01-20 22:36:57 +0900

    add

---
+++ rust-workspace/tools/test.py
@@ -0,0 +1,108 @@
+#!/opt/homebrew/bin/python3
+import os
+import json
+import argparse
+import subprocess
+import datetime
+
+dt_now = datetime.datetime.now()
+def read_file(path):
+    with open(path) as f: 
+        return f.read()
+
+def write_fault_test_result(test_case,got,expect):
+    with open("./result.txt","a") as f:
+        f.write(
+f"""
+{"=" * 100}
+{test_case} {datetime.datetime.now().time()}
+{"=" * 100}
+{got}
+
+
+"""
+        )
+    pass
+
+def red_s(s):
+    return f"\033[31m{s}\033[0m"
+def green_s(s):
+    return f"\033[32m{s}\033[0m"
+
+def check_test(test_in_file,test_out_file,binfile):
+    stdin = read_file(test_in_file)
+    expect = read_file(test_out_file)
+    stdout=""
+    return_code=0
+    try:
+        cp = subprocess.run(
+            [binfile], 
+            input=stdin,
+            # capture_output=True, text=True, 
+            stdout=subprocess.PIPE,
+            encoding='UTF-8',
+            timeout=1.5
+        )
+        stdout=cp.stdout 
+        return_code = cp.returncode
+    except subprocess.TimeoutExpired:
+        print(f"{red_s("timeout")} :", test_in_file)
+        return
+
+    if return_code != 0:
+        print(f"{red_s("return_code")}: ", return_code, test_in_file)
+        return False
+        
+    if expect != stdout:
+        print(f"{red_s("fault")} test:", test_in_file)
+        write_fault_test_result(test_in_file,stdout,expect)
+        return False
+    else:
+        print(f"{green_s("pass")} test:",test_in_file)
+        return True
+
+def get_num_tests():
+    test_dir_files = os.listdir(f".tests")
+    return len(test_dir_files) / 2
+
+def compile():
+    subprocess.run(
+        ["cargo","build"]
+    )
+
+def main():
+    task=input("task:")
+    download_test(task)
+    num_tests = int(get_num_tests())
+
+    compile()
+    bin_file = "./target/debug/rust-workspace"
+
+    for i in range(1,num_tests + 1):
+        check_test(f".tests/sample-{i}.in",f".tests/sample-{i}.out",bin_file)
+
+def download_test(task):
+    data = {}
+    with open("./data.json") as f:
+        data = json.load(f)
+    
+    url = f"https://atcoder.jp/contests/{data["contest_id"]}/tasks/{data["contest_id"]}_{task}"
+
+    subprocess.run(["rm", "-r",".tests"])
+    os.mkdir(".tests")
+
+    cp = subprocess.run(
+        ["oj","download","-d", ".tests/",url],
+        encoding='UTF-8',
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+    )
+    
+    if cp.returncode == 0:
+        print(green_s("success download tests!"))
+    else:
+        print(green_s("fail download tests!"))
+        print(cp.stdout,cp.stderr)
+
+if __name__ == "__main__":
+    main()
