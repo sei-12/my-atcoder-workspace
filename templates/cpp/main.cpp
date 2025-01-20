f2395229c75e49cb1f572754a720da96968a5b35
sei-12 <sei.t11@icloud.com>
2025-01-20 15:12:06 +0900

    add template cpp

---
+++ templates/cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <../mylib/mylib.hpp>
+#include <stdio.h>
+#include <iostream>
+
+void solve(){
+
+}
+
+int main(){
+    solve();
+    return 0;
+}
---
+++ templates/cpp/mylib/mylib.cpp
@@ -0,0 +1,29 @@
+// 可変長引数マクロの定義
+#define GET_MACRO(_1,_2,_3,_4,NAME,...) NAME
+
+/**
+ * @brief 可変長引数マクロを使用して、Pythonのrangeのような挙動を提供します。
+ *
+ * @param i ループカウンタ変数。各ループで指定された範囲の値を持ちます。
+ * @param end 終了値。ループはこの値より小さい間繰り返されます (step > 0 の場合)。
+ * @param start 開始値 (オプション)。デフォルトは 0。
+ * @param step 増分値 (オプション)。デフォルトは 1。負の値を指定すると降順のループを実現できます。 0 の場合は無限ループになります。適切な値を設定してください。
+ */
+#define range(...) GET_MACRO(__VA_ARGS__, range3, range2, range1)(__VA_ARGS__)
+
+// range(start, end, step)
+#define range3(i, start, end, step) \
+    for (int i = (start); ((step) > 0 ? i < (end) : i > (end)); i += (step))
+
+// range(start, end) -> デフォルトで step = 1
+#define range2(i, start, end) \
+    for (int i = (start); i < (end); i++)
+
+// range(end) -> デフォルトで start = 0, step = 1
+#define range1(i, end) \
+    for (int i = 0; i < (end); i++)
+
+
+
+
+
