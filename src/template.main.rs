09cff6b3b421ce9195344c100e592fc5412b5b8b
sei-12 <sei.t11@icloud.com>
2025-02-10 22:45:56 +0900

    update template

--- src/template.main.rs
+++ src/template.main.rs
@@ -1,15 +1,18 @@
 #![allow(non_snake_case)]
 #![allow(unused)]
 
+use itertools::Itertools;
 use proconio::{
     input,
     marker::{Chars, Usize1},
 };
-use std::{
-    cmp::{max, min},
-    collections::{BinaryHeap, HashMap, VecDeque},
-};
-use itertools::Itertools;
+use std::collections::{BinaryHeap, HashMap, VecDeque};
+
+#[rustfmt::skip]
+fn max<T: Ord>(a: T, b: T) -> T { if a > b { a } else { b } }
+#[rustfmt::skip]
+fn min<T: Ord>(a: T, b: T) -> T { if a < b { a } else { b } }
+
 
 fn main() {
     input! {
